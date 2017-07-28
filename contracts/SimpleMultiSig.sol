18af2c22444cb3b3a62bc5bb9a4d065f5b512a15
Christian Lundkvist <christian.lundkvist@gmail.com>
2017-07-28 00:27:07 -0400

    Iterating on contract design

--- contracts/SimpleMultiSig.sol
+++ contracts/SimpleMultiSig.sol
@@ -1,47 +1,46 @@
-pragma solidity ^0.4.0;
+pragma solidity 0.4.11;
 contract SimpleMultiSig {
 
-    // Mutable state
-    uint public nonce;
-    
-    // Constant state
-    uint public threshold;
-    address[] public signers;
+    uint public nonce; // long-term mutable state
+    mapping(address => uint) hasSigned; //temp mutable state
+    uint public threshold; // immutable state
+    address[] public owners; // immutable state
+    mapping(address => bool) public isOwner; // immutable state
 
-    function SimpleMultiSig(uint threshold_, address[] signers_) {
-        // No more than 20 signers
-        if (signers_.length > 20) {throw;}
+    function SimpleMultiSig(uint threshold_, address[] owners_) {
+        if (owners_.length > 10) {throw;}
+
+        for (uint i=0; i<owners_.length; i++) {
+            if(owners_[i] == 0) {throw;} // disallow zero address
+            if(isOwner[owners_[i]]) {throw;} // disallow duplicates
+            isOwner[owners_[i]] = true;
+        }
+
         threshold = threshold_;
-        signers = signers_;
+        owners = owners_;
         nonce = 0;
     }
     
-    function sendTransaction(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, uint nonce_, address destination, uint value, bytes32 data) {
-
+    function sendTransaction(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, address destination, uint value, bytes32 data) {
         if (sigR.length != threshold) {throw;}
         if (sigR.length != sigS.length || sigR.length != sigV.length) {throw;}
-        if (nonce_ != nonce) {throw;}
 
-        mapping(address => uint) signatures;
-        bytes32 txHash = sha3(bytes32(nonce), bytes32(destination), bytes32(value), data);
-        uint i=0;
+        bytes32 txHash = sha3(this, bytes32(nonce), bytes32(destination), bytes32(value), data);
 
-        for (i=0; i<threshold; i++) {
+        for (uint i=0; i<threshold; i++) {
             address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
-            signatures[recovered] = 1;
+            if (!isOwner[recovered]) {throw;} // Signer is not owner or sig invalid
+            if (hasSigned[recovered] != 0) {throw;} // Duplicate sig
+            hasSigned[recovered] = 1;
         }
-        
-        uint sumOfSigners = 0;        
-        for (i=0; i<threshold; i++) {
-            sumOfSigners = sumOfSigners + signatures[signers[i]];
+
+        for (i=0; i<owners.length; i++) {
+            hasSigned[owners[i]] = 0; // zero out temp storage
         }
-        
-        if (sumOfSigners == threshold) {
-            nonce = nonce + 1;
-            if (!destination.call.value(value)(data)) {
-                throw;
-            }
-        }
+
+        // If we make it here all signatures are good
+        nonce = nonce + 1;
+        if (!destination.call.value(value)(data)) {throw;}
     }
     
     function () payable {}
