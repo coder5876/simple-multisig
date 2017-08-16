c64515653bd2848cd1b5557e0300c7b3249705ef
maurelian <maurelian@protonmail.ch>
2017-08-15 17:10:57 -0700

    Update to use require instead of throw

--- contracts/SimpleMultiSig.sol
+++ contracts/SimpleMultiSig.sol
@@ -7,7 +7,7 @@ contract SimpleMultiSig {
   address[] public ownersArr;        // immutable state
 
   function SimpleMultiSig(uint threshold_, address[] owners_) {
-    if (owners_.length > 10 || threshold_ > owners_.length || threshold_ == 0) {throw;}
+    require(owners_.length <= 10 && threshold_ <= owners_.length && threshold_ != 0);
 
     for (uint i=0; i<owners_.length; i++) {
       isOwner[owners_[i]] = true;
@@ -18,8 +18,8 @@ contract SimpleMultiSig {
 
   // Note that address recovered from signatures must be strictly increasing
   function execute(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, address destination, uint value, bytes data) {
-    if (sigR.length != threshold) {throw;}
-    if (sigR.length != sigS.length || sigR.length != sigV.length) {throw;}
+    require(sigR.length == threshold);
+    require(sigR.length == sigS.length && sigR.length == sigV.length);
 
     // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
     bytes32 txHash = sha3(byte(0x19), byte(0), this, destination, value, data, nonce);
@@ -27,13 +27,13 @@ contract SimpleMultiSig {
     address lastAdd = address(0); // cannot have address(0) as an owner
     for (uint i = 0; i < threshold; i++) {
         address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
-        if (recovered <= lastAdd || !isOwner[recovered]) throw;
+        require(recovered > lastAdd && isOwner[recovered]);
         lastAdd = recovered;
     }
 
     // If we make it here all signatures are accounted for
     nonce = nonce + 1;
-    if (!destination.call.value(value)(data)) {throw;}
+    require(destination.call.value(value)(data));
   }
 
   function () payable {}
