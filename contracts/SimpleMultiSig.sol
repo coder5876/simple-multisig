d84f6f2ccf15aa6f820aacd6816f65b99c0f97ee
Andres Gonzalez <andres@3months.com>
2018-01-19 16:40:17 +1300

    fixed tests, updated solidity version

--- contracts/SimpleMultiSig.sol
+++ contracts/SimpleMultiSig.sol
@@ -1,43 +1,51 @@
-pragma solidity 0.4.15;
+pragma solidity 0.4.18;
+
+
 contract SimpleMultiSig {
+    uint public nonce;          // (only) mutable state
+    uint public threshold;      // immutable state
+    mapping (address => bool) public isOwner; // immutable state
+    address[] public ownersArr;        // immutable state
 
-  uint public nonce;                // (only) mutable state
-  uint public threshold;            // immutable state
-  mapping (address => bool) isOwner; // immutable state
-  address[] public ownersArr;        // immutable state
+    function SimpleMultiSig(uint threshold_, address[] owners_) public {
+        require(owners_.length <= 10 && threshold_ <= owners_.length && threshold_ != 0);
 
-  function SimpleMultiSig(uint threshold_, address[] owners_) {
-    require(owners_.length <= 10 && threshold_ <= owners_.length && threshold_ != 0);
-
-    address lastAdd = address(0); 
-    for (uint i=0; i<owners_.length; i++) {
-      require(owners_[i] > lastAdd);
-      isOwner[owners_[i]] = true;
-      lastAdd = owners_[i];
-    }
-    ownersArr = owners_;
-    threshold = threshold_;
-  }
-
-  // Note that address recovered from signatures must be strictly increasing
-  function execute(uint8[] sigV, bytes32[] sigR, bytes32[] sigS, address destination, uint value, bytes data) {
-    require(sigR.length == threshold);
-    require(sigR.length == sigS.length && sigR.length == sigV.length);
-
-    // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
-    bytes32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce);
-
-    address lastAdd = address(0); // cannot have address(0) as an owner
-    for (uint i = 0; i < threshold; i++) {
-        address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
-        require(recovered > lastAdd && isOwner[recovered]);
-        lastAdd = recovered;
+        address lastAdd = address(0);
+        for (uint i=0; i < owners_.length; i++) {
+            require(owners_[i] > lastAdd);
+            isOwner[owners_[i]] = true;
+            lastAdd = owners_[i];
+        }
+        ownersArr = owners_;
+        threshold = threshold_;
     }
 
-    // If we make it here all signatures are accounted for
-    nonce = nonce + 1;
-    require(destination.call.value(value)(data));
-  }
+    function () public payable {}
 
-  function () payable {}
+    // Note that address recovered from signatures must be strictly increasing
+    function execute(
+        uint8[] sigV,
+        bytes32[] sigR,
+        bytes32[] sigS,
+        address destination,
+        uint value,
+        bytes data)
+    public {
+        require(sigR.length == threshold);
+        require(sigR.length == sigS.length && sigR.length == sigV.length);
+
+        // Follows ERC191 signature scheme: https://github.com/ethereum/EIPs/issues/191
+        bytes32 txHash = keccak256(byte(0x19), byte(0), this, destination, value, data, nonce);
+
+        address lastAdd = address(0); // cannot have address(0) as an owner
+        for (uint i = 0; i < threshold; i++) {
+            address recovered = ecrecover(txHash, sigV[i], sigR[i], sigS[i]);
+            require(recovered > lastAdd && isOwner[recovered]);
+            lastAdd = recovered;
+        }
+
+        // If we make it here all signatures are accounted for
+        nonce = nonce + 1;
+        require(destination.call.value(value)(data));
+    }
 }
--- test/simplemultisig.js
+++ test/simplemultisig.js
@@ -1,239 +1,332 @@
-var SimpleMultiSig = artifacts.require("./SimpleMultiSig.sol")
-var TestRegistry = artifacts.require("./TestRegistry.sol")
-var lightwallet = require('eth-lightwallet')
-const solsha3 = require('solidity-sha3').default
-const leftPad = require('left-pad')
-const Promise = require('bluebird')
-const BigNumber = require('bignumber.js')
+var SimpleMultiSig = artifacts.require("./SimpleMultiSig.sol");
+var TestRegistry = artifacts.require("./TestRegistry.sol");
+var lightwallet = require("eth-lightwallet");
+const solsha3 = require("solidity-sha3").default;
+const leftPad = require("left-pad");
+const Promise = require("bluebird");
+const BigNumber = require("bignumber.js");
 
-const web3SendTransaction = Promise.promisify(web3.eth.sendTransaction)
-const web3GetBalance = Promise.promisify(web3.eth.getBalance)
+const web3SendTransaction = Promise.promisify(web3.eth.sendTransaction);
+const web3GetBalance = Promise.promisify(web3.eth.getBalance);
 
-contract('SimpleMultiSig', function(accounts) {
+contract("SimpleMultiSig", function(accounts) {
+  let keyFromPw;
+  let acct;
+  let lw;
 
-  let keyFromPw
-  let acct
-  let lw
+  let createSigs = function(
+    signers,
+    multisigAddr,
+    nonce,
+    destinationAddr,
+    value,
+    data
+  ) {
+    let input =
+      "0x19" +
+      "00" +
+      multisigAddr.slice(2) +
+      destinationAddr.slice(2) +
+      leftPad(value.toString("16"), "64", "0") +
+      data.slice(2) +
+      leftPad(nonce.toString("16"), "64", "0");
+    let hash = solsha3(input);
 
-  let createSigs = function(signers, multisigAddr, nonce, destinationAddr, value, data) {
+    let sigV = [];
+    let sigR = [];
+    let sigS = [];
 
-    let input = '0x19' + '00' + multisigAddr.slice(2) + destinationAddr.slice(2) + leftPad(value.toString('16'), '64', '0') + data.slice(2) + leftPad(nonce.toString('16'), '64', '0')
-    let hash = solsha3(input)
-
-    let sigV = []
-    let sigR = []
-    let sigS = []
-
-    for (var i=0; i<signers.length; i++) {
-      let sig = lightwallet.signing.signMsgHash(lw, keyFromPw, hash, signers[i])
-      sigV.push(sig.v)
-      sigR.push('0x' + sig.r.toString('hex'))
-      sigS.push('0x' + sig.s.toString('hex'))
+    for (var i = 0; i < signers.length; i++) {
+      let sig = lightwallet.signing.signMsgHash(
+        lw,
+        keyFromPw,
+        hash,
+        signers[i]
+      );
+      sigV.push(sig.v);
+      sigR.push("0x" + sig.r.toString("hex"));
+      sigS.push("0x" + sig.s.toString("hex"));
     }
 
-    return {sigV: sigV, sigR: sigR, sigS: sigS}
-
-  }
+    return { sigV: sigV, sigR: sigR, sigS: sigS };
+  };
 
   let executeSendSuccess = async function(owners, threshold, signers, done) {
+    let multisig = await SimpleMultiSig.new(threshold, owners, {
+      from: accounts[0]
+    });
 
-    let multisig = await SimpleMultiSig.new(threshold, owners, {from: accounts[0]})
-
-    let randomAddr = solsha3(Math.random()).slice(0,42)
+    let randomAddr = solsha3(Math.random()).slice(0, 42);
 
     // Receive funds
-    await web3SendTransaction({from: accounts[0], to: multisig.address, value: web3.toWei(new BigNumber(0.1), 'ether')})
+    await web3SendTransaction({
+      from: accounts[0],
+      to: multisig.address,
+      value: web3.toWei(new BigNumber(0.1), "ether")
+    });
 
-    let nonce = await multisig.nonce.call()
-    assert.equal(nonce.toNumber(), 0)
+    let nonce = await multisig.nonce.call();
+    assert.equal(nonce.toNumber(), 0);
 
-    let bal = await web3GetBalance(multisig.address)
-    assert.equal(bal, web3.toWei(0.1, 'ether'))
+    let bal = await web3GetBalance(multisig.address);
+    assert.equal(bal, web3.toWei(0.1, "ether"));
 
     // check that owners are stored correctly
-    for (var i=0; i<owners.length; i++) {
-      let ownerFromContract = await multisig.ownersArr.call(i)
-      assert.equal(owners[i], ownerFromContract)
+    for (var i = 0; i < owners.length; i++) {
+      let ownerFromContract = await multisig.ownersArr.call(i);
+      assert.equal(owners[i], ownerFromContract);
     }
 
-    let value = web3.toWei(new BigNumber(0.01), 'ether')
+    let value = web3.toWei(new BigNumber(0.01), "ether");
 
-    let sigs = createSigs(signers, multisig.address, nonce, randomAddr, value, '0x')
+    let sigs = createSigs(
+      signers,
+      multisig.address,
+      nonce,
+      randomAddr,
+      value,
+      "0x"
+    );
 
-    await multisig.execute(sigs.sigV, sigs.sigR, sigs.sigS, randomAddr, value, '0x', {from: accounts[0], gasLimit: 1000000})
+    await multisig.execute(
+      sigs.sigV,
+      sigs.sigR,
+      sigs.sigS,
+      randomAddr,
+      value,
+      "0x",
+      { from: accounts[0], gasLimit: 1000000 }
+    );
 
     // Check funds sent
-    bal = await web3GetBalance(randomAddr)
-    assert.equal(bal.toString(), value.toString())
+    bal = await web3GetBalance(randomAddr);
+    assert.equal(bal.toString(), value.toString());
 
     // Check nonce updated
-    nonce = await multisig.nonce.call()
-    assert.equal(nonce.toNumber(), 1)
+    nonce = await multisig.nonce.call();
+    assert.equal(nonce.toNumber(), 1);
 
     // Send again
-    sigs = createSigs(signers, multisig.address, nonce, randomAddr, value, '0x')
-    await multisig.execute(sigs.sigV, sigs.sigR, sigs.sigS, randomAddr, value, '0x', {from: accounts[0], gasLimit: 1000000})
+    sigs = createSigs(
+      signers,
+      multisig.address,
+      nonce,
+      randomAddr,
+      value,
+      "0x"
+    );
+    await multisig.execute(
+      sigs.sigV,
+      sigs.sigR,
+      sigs.sigS,
+      randomAddr,
+      value,
+      "0x",
+      { from: accounts[0], gasLimit: 1000000 }
+    );
 
     // Check funds
-    bal = await web3GetBalance(randomAddr)
-    assert.equal(bal.toString(), (value*2).toString())
+    bal = await web3GetBalance(randomAddr);
+    assert.equal(bal.toString(), (value * 2).toString());
 
     // Check nonce updated
-    nonce = await multisig.nonce.call()
-    assert.equal(nonce.toNumber(), 2)
+    nonce = await multisig.nonce.call();
+    assert.equal(nonce.toNumber(), 2);
 
     // Test contract interactions
-    let reg = await TestRegistry.new({from: accounts[0]})
+    let reg = await TestRegistry.new({ from: accounts[0] });
 
-    let number = 12345
-    let data = '0x' + lightwallet.txutils._encodeFunctionTxData('register', ['uint256'], [number])
+    let number = 12345;
+    let data = lightwallet.txutils._encodeFunctionTxData(
+      "register",
+      ["uint256"],
+      [number]
+    );
 
-    sigs = createSigs(signers, multisig.address, nonce, reg.address, value, data)
-    await multisig.execute(sigs.sigV, sigs.sigR, sigs.sigS, reg.address, value, data, {from: accounts[0], gasLimit: 1000000})
+    sigs = createSigs(
+      signers,
+      multisig.address,
+      nonce,
+      reg.address,
+      value,
+      data
+    );
+    await multisig.execute(
+      sigs.sigV,
+      sigs.sigR,
+      sigs.sigS,
+      reg.address,
+      value,
+      data,
+      { from: accounts[0], gasLimit: 1000000 }
+    );
 
     // Check that number has been set in registry
-    let numFromRegistry = await reg.registry(multisig.address)
-    assert.equal(numFromRegistry.toNumber(), number)
+    let numFromRegistry = await reg.registry(multisig.address);
+    assert.equal(numFromRegistry.toNumber(), number);
 
     // Check funds in registry
-    bal = await web3GetBalance(reg.address)
-    assert.equal(bal.toString(), value.toString())
+    bal = await web3GetBalance(reg.address);
+    assert.equal(bal.toString(), value.toString());
 
     // Check nonce updated
-    nonce = await multisig.nonce.call()
-    assert.equal(nonce.toNumber(), 3)
+    nonce = await multisig.nonce.call();
+    assert.equal(nonce.toNumber(), 3);
 
-    done()
-  }
+    done();
+  };
 
   let executeSendFailure = async function(owners, threshold, signers, done) {
+    let multisig = await SimpleMultiSig.new(threshold, owners, {
+      from: accounts[0]
+    });
 
-    let multisig = await SimpleMultiSig.new(threshold, owners, {from: accounts[0]})
+    let nonce = await multisig.nonce.call();
 
-    let nonce = await multisig.nonce.call()
-    assert.equal(nonce.toNumber(), 0)
+    assert.equal(nonce.toNumber(), 0);
 
     // Receive funds
-    await web3SendTransaction({from: accounts[0], to: multisig.address, value: web3.toWei(new BigNumber(2), 'ether')})
+    await web3SendTransaction({
+      from: accounts[0],
+      to: multisig.address,
+      value: web3.toWei(new BigNumber(2), "ether")
+    });
 
-    let randomAddr = solsha3(Math.random()).slice(0,42)
-    let value = web3.toWei(new BigNumber(0.1), 'ether')
-    let sigs = createSigs(signers, multisig.address, nonce, randomAddr, value, '0x')
+    let randomAddr = solsha3(Math.random()).slice(0, 42);
+    let value = web3.toWei(new BigNumber(0.1), "ether");
+    let sigs = createSigs(
+      signers,
+      multisig.address,
+      nonce,
+      randomAddr,
+      value,
+      "0x"
+    );
 
-    let errMsg = ''
+    let errMsg = "";
     try {
-    await multisig.execute(sigs.sigV, sigs.sigR, sigs.sigS, randomAddr, value, '0x', {from: accounts[0], gasLimit: 1000000})
-    }
-    catch(error) {
-      errMsg = error.message
+      await multisig.execute(
+        sigs.sigV,
+        sigs.sigR,
+        sigs.sigS,
+        randomAddr,
+        value,
+        "0x",
+        { from: accounts[0], gasLimit: 1000000 }
+      );
+    } catch (error) {
+      errMsg = error.message;
     }
 
-    assert.equal(errMsg, 'VM Exception while processing transaction: invalid opcode', 'Test did not throw')
+    assert.equal(
+      errMsg,
+      "VM Exception while processing transaction: invalid opcode",
+      "Test did not throw"
+    );
 
-    done()
-  }
+    done();
+  };
 
   let creationFailure = async function(owners, threshold, done) {
-
     try {
-      await SimpleMultiSig.new(threshold, owners, {from: accounts[0]})
+      await SimpleMultiSig.new(threshold, owners, { from: accounts[0] });
+    } catch (error) {
+      errMsg = error.message;
     }
-    catch(error) {
-      errMsg = error.message
-    }
 
-    assert.equal(errMsg, 'VM Exception while processing transaction: invalid opcode', 'Test did not throw')
+    assert.equal(
+      errMsg,
+      "VM Exception while processing transaction: invalid opcode",
+      "Test did not throw"
+    );
 
-    done()
-  }
-  
-  before((done) => {
+    done();
+  };
 
-    let seed = "pull rent tower word science patrol economy legal yellow kit frequent fat"
+  before(done => {
+    let seed =
+      "pull rent tower word science patrol economy legal yellow kit frequent fat";
 
     lightwallet.keystore.createVault(
-    {hdPathString: "m/44'/60'/0'/0",
-     seedPhrase: seed,
-     password: "test",
-     salt: "testsalt"
-    },
-    function (err, keystore) {
+      {
+        hdPathString: "m/44'/60'/0'/0",
+        seedPhrase: seed,
+        password: "test",
+        salt: "testsalt"
+      },
+      function(err, keystore) {
+        lw = keystore;
+        lw.keyFromPassword("test", function(e, k) {
+          keyFromPw = k;
 
-      lw = keystore
-      lw.keyFromPassword("test", function(e,k) {
-        keyFromPw = k
-
-        lw.generateNewAddress(keyFromPw, 20)
-        let acctWithout0x = lw.getAddresses()
-        acct = acctWithout0x.map((a) => {return '0x'+a})
-        acct.sort()
-        done()
-      })
-    })
-  })
+          lw.generateNewAddress(keyFromPw, 20);
+          acct = lw.getAddresses();
+          acct.sort();
+          done();
+        });
+      }
+    );
+  });
 
   describe("3 signers, threshold 2", () => {
+    it("should succeed with signers 0, 1", done => {
+      let signers = [acct[0], acct[1]];
+      signers.sort();
+      executeSendSuccess(acct.slice(0, 3), 2, signers, done);
+    });
 
-    it("should succeed with signers 0, 1", (done) => {
-      let signers = [acct[0], acct[1]]
-      signers.sort()
-      executeSendSuccess(acct.slice(0,3), 2, signers, done)
-    })
+    it("should succeed with signers 0, 2", done => {
+      let signers = [acct[0], acct[2]];
+      signers.sort();
+      executeSendSuccess(acct.slice(0, 3), 2, signers, done);
+    });
 
-    it("should succeed with signers 0, 2", (done) => {
-      let signers = [acct[0], acct[2]]
-      signers.sort()
-      executeSendSuccess(acct.slice(0,3), 2, signers, done)
-    })
+    it("should succeed with signers 1, 2", done => {
+      let signers = [acct[1], acct[2]];
+      signers.sort();
+      executeSendSuccess(acct.slice(0, 3), 2, signers, done);
+    });
 
-    it("should succeed with signers 1, 2", (done) => {
-      let signers = [acct[1], acct[2]]
-      signers.sort()
-      executeSendSuccess(acct.slice(0,3), 2, signers, done)
-    })
+    it("should fail due to non-owner signer", done => {
+      let signers = [acct[0], acct[3]];
+      signers.sort();
+      executeSendFailure(acct.slice(0, 3), 2, signers, done);
+    });
 
-    it("should fail due to non-owner signer", (done) => {
-      let signers = [acct[0], acct[3]]
-      signers.sort()
-      executeSendFailure(acct.slice(0,3), 2, signers, done)
-    })
+    it("should fail with more signers than threshold", done => {
+      executeSendFailure(acct.slice(0, 3), 2, acct.slice(0, 3), done);
+    });
 
-    it("should fail with more signers than threshold", (done) => {
-      executeSendFailure(acct.slice(0,3), 2, acct.slice(0,3), done)
-    })
+    it("should fail with fewer signers than threshold", done => {
+      executeSendFailure(acct.slice(0, 3), 2, [acct[0]], done);
+    });
 
-    it("should fail with fewer signers than threshold", (done) => {
-      executeSendFailure(acct.slice(0,3), 2, [acct[0]], done)
-    })
+    it("should fail with one signer signing twice", done => {
+      executeSendFailure(acct.slice(0, 3), 2, [acct[0], acct[0]], done);
+    });
 
-    it("should fail with one signer signing twice", (done) => {
-      executeSendFailure(acct.slice(0,3), 2, [acct[0], acct[0]], done)
-    })
-
-    it("should fail with signers in wrong order", (done) => {
-      let signers = [acct[0], acct[1]]
-      signers.sort().reverse() //opposite order it should be
-      executeSendFailure(acct.slice(0,3), 2, signers, done)
-    })
-
-  })  
+    it("should fail with signers in wrong order", done => {
+      let signers = [acct[0], acct[1]];
+      signers.sort().reverse(); //opposite order it should be
+      executeSendFailure(acct.slice(0, 3), 2, signers, done);
+    });
+  });
 
   describe("Edge cases", () => {
-    it("should succeed with 10 owners, 10 signers", (done) => {
-      executeSendSuccess(acct.slice(0,10), 10, acct.slice(0,10), done)
-    })
+    it("should succeed with 10 owners, 10 signers", done => {
+      executeSendSuccess(acct.slice(0, 10), 10, acct.slice(0, 10), done);
+    });
 
-    it("should fail to create with signers 0, 0, 2, and threshold 3", (done) => { 
-      creationFailure([acct[0],acct[0],acct[2]], 3, done)
-    })
+    it("should fail to create with signers 0, 0, 2, and threshold 3", done => {
+      creationFailure([acct[0], acct[0], acct[2]], 3, done);
+    });
 
-    it("should fail with 0 signers", (done) => {
-      executeSendFailure(acct.slice(0,3), 2, [], done)
-    })
+    it("should fail with 0 signers", done => {
+      executeSendFailure(acct.slice(0, 3), 2, [], done);
+    });
 
-    it("should fail with 11 owners", (done) => {
-      creationFailure(acct.slice(0,11), 2, done)
-    })
-  })
-
-})
+    it("should fail with 11 owners", done => {
+      creationFailure(acct.slice(0, 11), 2, done);
+    });
+  });
+});
